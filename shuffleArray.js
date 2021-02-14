58504192aeac51d3a0fd33c9ef718a30cfd4927b
Nagalingam Vibbehsan <vibbeshan@gmail.com>
2021-02-14 13:52:22 +0100

    ;

--- shuffleArray.js
+++ shuffleArray.js
@@ -4,25 +4,25 @@ class ShuffleArray {
     constructor(array){
         this._array = array;
         this._shuffledArray = [];
-        this.arrayShuffle(this._array)
+        this.arrayShuffle(this._array);
     }
 
     get shuffledArray(){
-        return [...this._shuffledArray]
+        return [...this._shuffledArray];
     }
 
      /*arrayShuffle(array datas) --> none
     Mélange les données de l'array ( algorithme mélange de Fisher-Yates )*/
     arrayShuffle(datas){
-        let dataArray = [...datas]
+        let dataArray = [...datas];
         for( let i = dataArray.length - 1; i > 0; i--){
             let j = Math.floor(Math.random()*(i+1));
             let actualElement = dataArray[i];
             dataArray[i] = dataArray[j];
             dataArray[j] = actualElement;
-        }
-        this._shuffledArray = [...dataArray]
+        };
+        this._shuffledArray = [...dataArray];
     console.log(dataArray, this.shuffledArray)
-    }
+    };
 
-}
+};
